1c18196116fceb60c31b028c1f1e51ce2438f832
Oscar <oscararnu18@gmail.com>
2025-03-09 19:48:42 +0100

    server configured 2.0

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import { serve } from "@hono/node-server";
-import { serveStatic } from "hono/cloudflare-workers";
+import { serveStatic } from "hono/middleware";
 import { characters } from "./mock-data.js";
 import { CharacterListResponse } from "./model.js";
 
